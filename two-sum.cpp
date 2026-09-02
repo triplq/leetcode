93d61454266b76c8b10550287e910f6ccdf8ecaa
Алексей Ярошенко <aleksejarosenko@MacBook-Pro-Aleksej.local>
2026-09-02 14:52:38 +0300

    02 complete but bad time

--- two-sum.cpp
+++ two-sum.cpp
@@ -6,6 +6,23 @@ using namespace std;
 class Solution{
     public:
         vector<int> twoSum(vector<int>& nums, int target){
-            
+            for(int i = 0; i < nums.size(); i++){
+                int search = target-nums[i];
+                int j = i+1;
+                while(j < nums.size() && nums[j] != search){
+                    j++;
+                }
+                if (j < nums.size()){
+                    return vector<int> {i, j};
+                }
+            }
         }
 };
+
+int main(){
+    Solution sol;
+    vector<int> num {3,2,4};
+    u_int target = 6;
+
+    sol.twoSum(num, target);
+}
