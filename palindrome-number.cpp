8a58c78130f2fab73f7c4d86aa88ff0ec47cdc32
Алексей Ярошенко <aleksejarosenko@MacBook-Pro-Aleksej.local>
2026-09-02 18:01:33 +0300

    02 good mem bad time

--- palindrome-number.cpp
+++ palindrome-number.cpp
@@ -2,13 +2,29 @@
 
 class Solution{
     public:
-        bool isPalindrome(int x){
+        int isPalindrome(int x){
+            if (x<0) return false;
+            long long int d = 1;
+            long long int a = 0;
+            while (x / d - 10 >= 0){
+                d *= 10;
+            }
+            d*=10;
+            
+            for (long long int i = 1; i < d; i*=10){
+                a += x%(i*10)/i;
+                a*=10;
+            }
 
+            return a/10;
         }
 };
 
 int main(){
     Solution sol;
 
-    cout << sol.isPalindrome() << '\n';
+    // std::cout << 12345%100/10 << '\n';
+
+
+    std::cout << sol.isPalindrome(12345) << '\n';
 }
