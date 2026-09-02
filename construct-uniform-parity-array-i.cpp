1c31b9469e30b6000bf7110f50e6956f487b819e
Алексей Ярошенко <aleksejarosenko@MacBook-Pro-Aleksej.local>
2026-09-02 14:08:12 +0300

    completed

--- construct-uniform-parity-array-i.cpp
+++ construct-uniform-parity-array-i.cpp
@@ -6,29 +6,15 @@ using namespace std;
 class Solution{ 
     public:
         bool uniformArray(vector<int>& nums1){
-            if (nums1.size() == 1){
-                return true;
-            }
-
-            vector<int> nums2;
-            u_int n = nums1.size();
-            for(u_int i = 1; i < n; i++){
-                if(nums1[i] % 2 == 0 & nums1[i-1] % 2 != 0 || nums1[i] % 2 != 0 & nums1[i-1] % 2 == 0){
-                    continue;
-                }else{
-                    return true;
-                }
-            }
-
-            return false;
+            return true;
         }
 };
 
 int main(){
     Solution sol;
-    vector<int> num {4,3};
+    vector<int> num {2,4,6,8};
 
-    cout << sol.uniformArray(num) << '\n';
+    cout << (sol.uniformArray(num) ? "true" : "false") << '\n';
 
     return 0;
 }
