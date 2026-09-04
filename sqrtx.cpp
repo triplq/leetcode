6f196f65f26844a7de2be7dcaaa7fcbac5e4a99d
Алексей Ярошенко <aleksejarosenko@MacBook-Pro-Aleksej.local>
2026-09-04 14:29:39 +0300

    07 start

---
+++ sqrtx.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+public:
+    int mySqrtx(int x){
+
+    }
+};
+
+int main(){
+    Solution sol;
+
+    cout << sol.mySqrtx(4) << '\n';
+
+    return 0;
+}
