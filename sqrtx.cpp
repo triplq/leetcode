1171ef69ad018398c5bb81d5dc18f4e8c174722b
triplq <alexy.yaroshenko05@icloud.com>
2026-09-04 16:55:34 +0300

    07 theory

--- sqrtx.cpp
+++ sqrtx.cpp
@@ -2,10 +2,16 @@
 
 using namespace std;
 
+/*we should use Newtons method specifically Hero's algorithm
+to calculate sqrt with:
+(xn^2+z)/(2*x0)=xn+1 where z- is our sqrt(x)
+x0 should be number that is bigger then resault of sqrt(x)
+=> this number shuold be x itslef*/
+
 class Solution{
 public:
     int mySqrtx(int x){
-
+        
     }
 };
 
