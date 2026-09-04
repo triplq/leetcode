6cd547d47a9977d74b7db985cd97ae380b40569d
Алексей Ярошенко <aleksejarosenko@MacBook-Pro-Aleksej.local>
2026-09-04 13:23:08 +0300

    06 start

---
+++ search-insert-position.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution{
+public:
+    int searchInsert(vector<int>& nums, int target){
+
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums {1,3,5,6};
+
+    cout << sol.searchInsert(nums, 5);
+
+    return 0;
+}
