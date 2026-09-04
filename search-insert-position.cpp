2b2df64526a67516994df3162d108b62a1028e52
Алексей Ярошенко <aleksejarosenko@MacBook-Pro-Aleksej.local>
2026-09-04 13:58:00 +0300

    06 complete

--- search-insert-position.cpp
+++ search-insert-position.cpp
@@ -2,10 +2,26 @@
 
 using namespace std;
 
+/*leftmost binary serach aka std::lower_bound
+search for middle of a massive
+if mid<x => left = mid+1
+else mid>=x => right = mid
+repeat*/
+
 class Solution{
 public:
     int searchInsert(vector<int>& nums, int target){
+        int left = 0; int right = nums.size();
+        int mid;
+        while(right != left){
+            mid = (right+left)/2;
+            if (nums[mid] < target)
+                left = mid+1;
+            else
+                right = mid;
+        }
 
+        return right;
     }
 };
 
@@ -13,7 +29,7 @@ int main(){
     Solution sol;
     vector<int> nums {1,3,5,6};
 
-    cout << sol.searchInsert(nums, 5);
+    cout << sol.searchInsert(nums, 2) << '\n';
 
     return 0;
 }
